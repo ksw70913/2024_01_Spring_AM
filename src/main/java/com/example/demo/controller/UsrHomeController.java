f7ea8300166c8013f212a3d52894129b5cf3a784
ksw70913 <pavico0913@gmail.com>
2024-01-26 17:18:23 +0900

    count 초기화

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -6,10 +6,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+
+	private int count;
+
+	public UsrHomeController() {
+		count = 0;
+	}
+
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		count = 0;
+		return "count의 값이 0으로 초기화 되었습니다.";
+	}
+
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		return count++;
+	}
+
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
-		return "안녕";
+		return "안녕하세요";
 	}
 
 	@RequestMapping("/usr/home/main2")
@@ -25,14 +45,4 @@ public class UsrHomeController {
 		int b = 2;
 		return a + b;
 	}
-
-	int i = -1;
-
-	@RequestMapping("/usr/home/main4")
-	@ResponseBody
-	public int showMain4() {
-		i++;
-		return i;
-	}
-
 }
