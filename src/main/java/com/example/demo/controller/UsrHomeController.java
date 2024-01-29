e22c0c6f056a5c86a9052b56b48c789e6e45f8fa
ksw70913 <pavico0913@gmail.com>
2024-01-29 13:33:26 +0900

    ArticleService 도입 및 기능 이전

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,19 +1,17 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Controller
 public class UsrHomeController {
 
-}	
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+
+		return "MAIN!";
+	}
+
+}
