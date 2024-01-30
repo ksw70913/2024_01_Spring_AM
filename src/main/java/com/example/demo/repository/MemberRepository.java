56212c3676c6764c83e04165cdd44fa5f77f20ce
ksw70913 <pavico0913@gmail.com>
2024-01-30 14:23:03 +0900

    아이디 중복 체크

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -8,18 +8,25 @@ import com.example.demo.vo.Member;
 
 @Mapper
 public interface MemberRepository {
-	@Insert("""
-			INSERT INTO
-			`member` SET
-			regDate = NOW(),
-			updateDate = NOW(),
-			loginId = #{loginId},
-			loginPw = #{loginPw},
-			`name` = #{name},
-			nickname = #{nickname},
-			cellphoneNum = #{cellphoneNum},
-			email = #{email}
+	@Select("""
+			SELECT *
+			FROM `member`
+			WHERE loginId = #{loginId}
 			""")
+	public Member getMemberByLoginId(String loginId);
+
+//	@Insert("""
+//			INSERT INTO
+//			`member` SET
+//			regDate = NOW(),
+//			updateDate = NOW(),
+//			loginId = #{loginId},
+//			loginPw = #{loginPw},
+//			`name` = #{name},
+//			nickname = #{nickname},
+//			cellphoneNum = #{cellphoneNum},
+//			email = #{email}
+//			""")
 	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email);
 
 	@Select("SELECT LAST_INSERT_ID()")
@@ -27,4 +34,5 @@ public interface MemberRepository {
 
 	@Select("SELECT * FROM `member` WHERE id = #{id}")
 	public Member getMember(int id);
+
 }
