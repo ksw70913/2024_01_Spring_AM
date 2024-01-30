6d5811fc1ed4343c115a291928d15721247a97c2
ksw70913 <pavico0913@gmail.com>
2024-01-30 17:23:56 +0900

    getArticles, doWrite에 ResultData 적용

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.demo.service.MemberService;
 import com.example.demo.util.Ut;
 import com.example.demo.vo.Member;
+import com.example.demo.vo.ResultData;
 
 @Controller
 public class UsrMemberController {
@@ -17,40 +18,40 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+	public ResultData doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
 		if (Ut.isNullOrEmpty(loginId)) {
-			return "아이디를 입력해주세요";
+			return ResultData.from("F-1", Ut.f("%s(아이디)가 제대로 입력되지 않았습니다.", loginId));
 		}
 		if (Ut.isNullOrEmpty(loginPw)) {
-			return "비밀번호를 입력해주세요";
+			return ResultData.from("F-1", Ut.f("%s(비밀번호)가 제대로 입력되지 않았습니다.", loginPw));
 		}
 		if (Ut.isNullOrEmpty(name)) {
-			return "이름을 입력해주세요";
+			return ResultData.from("F-1", Ut.f("%s(이름)이 제대로 입력되지 않았습니다.", name));
 		}
 		if (Ut.isNullOrEmpty(nickname)) {
-			return "닉네임을 입력해주세요";
+			return ResultData.from("F-1", Ut.f("%s(닉네임)이 제대로 입력되지 않았습니다.", nickname));
 		}
 		if (Ut.isNullOrEmpty(cellphoneNum)) {
-			return "전화번호를 입력해주세요";
+			return ResultData.from("F-1", Ut.f("%s(전화번호)가 제대로 입력되지 않았습니다.", cellphoneNum));
 		}
 		if (Ut.isNullOrEmpty(email)) {
-			return "이메일을 입력해주세요";
+			return ResultData.from("F-1", Ut.f("%s(이메일)이 제대로 입력되지 않았습니다.", email));
 		}
 
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
 
 		if (id == -1) {
-			return Ut.f("이미 사용중인 아이디(%s)입니다", loginId);
+			return ResultData.from("F-1", Ut.f("%s(아이디)가 이미 사용중입니다.", loginId));
 		}
 
 		if (id == -2) {
-			return Ut.f("이미 사용중인 이름(%s)과 이메일(%s)입니다", name, email);
+			return ResultData.from("F-1", Ut.f("이미 사용중인 이름(%s)과 이메일(%s)입니다", name, email));
 		}
 
 		Member member = memberService.getMember(id);
 
-		return member;
+		return ResultData.from("S-1", Ut.f("%d번째 회원이 생성되었습니다..", id), member);
 	}
 
 	@RequestMapping("/usr/member/doLogin")
